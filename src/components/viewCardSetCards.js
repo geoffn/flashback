18d60c436331d25a8e9235249a9158d761531879
Geoff Nelson <geoffreysnelson@gmail.com>
2021-02-25 06:56:43 -0800

    removed warning

--- src/components/viewCardSetCards.js
+++ src/components/viewCardSetCards.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react"
-import axios from 'axios'
 import anime from 'animejs'
 import NavBar from './Navbar'
 import { getCardsForCardset } from './helpers/CardSetHelper'
