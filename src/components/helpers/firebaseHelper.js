9dbefc674178b696b04f794e3915e3da6614a0fd
Geoff Nelson <geoffreysnelson@gmail.com>
2021-03-04 11:01:03 -0800

    added config

--- src/components/helpers/firebaseHelper.js
+++ src/components/helpers/firebaseHelper.js
@@ -3,8 +3,8 @@ import 'firebase/auth'
 import {getAuthConfig} from './ConfigHelper'
 
 const config = {
-    apiKey: 'AIzaSyDnFPmm_0LWGFv79Le86Cv69ZXeQYX4Gbk',
-    authDomain: 'flashback-d84b5.firebaseapp.com'
+    apiKey: ,
+    authDomain: 
 }
 
 
